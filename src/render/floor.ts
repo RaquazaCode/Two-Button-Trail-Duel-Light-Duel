51799a7dfe72dcbf93a10d06006a757eb160c5b3
Raquaza <raquazamoss@gmail.com>
2026-02-03 23:53:55 -0400

    feat: add reflective floor builder

---
+++ src/render/floor.ts
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+
+type FloorArgs = {
+  size: number;
+  envMap: THREE.Texture;
+  roughness: number;
+  envMapIntensity: number;
+  color: number;
+};
+
+export const createReflectiveFloor = (args: FloorArgs) => {
+  const geometry = new THREE.PlaneGeometry(args.size, args.size);
+  const material = new THREE.MeshStandardMaterial({
+    color: args.color,
+    metalness: 1,
+    roughness: args.roughness,
+    envMap: args.envMap,
+    envMapIntensity: args.envMapIntensity,
+  });
+
+  material.onBeforeCompile = (shader) => {
+    shader.fragmentShader = shader.fragmentShader.replace(
+      "#include <envmap_fragment>",
+      `#include <envmap_fragment>\n        float fresnel = pow(1.0 - abs(dot(normalize(normal), normalize(vViewPosition))), 3.0);\n        outgoingLight += envMapColor * fresnel * 0.35;`
+    );
+  };
+
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.rotation.x = -Math.PI / 2;
+  return mesh;
+};
